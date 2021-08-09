0c1542397f7fd6e7bff93124e6d033dc1aa00fe7
Oleksandr Mezhenskyi <oleksandr.mezhenskyi@chisw.com>
2021-08-09 22:38:10 +0300

    feat: add readable number from 0 to 99

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
+    if (number < 20 ) {
+        return units[number];
+    }
+
+    if (number >= 20 &&  number < 100) {
+        const numberOfTens = Math.floor(number / 10);
+        const numberOfUnits = number % 10;
+
+        return numberOfUnits ? `${tens[numberOfTens - 2]} ${units[numberOfUnits]}` : tens[numberOfTens - 2];
+    }
 }
