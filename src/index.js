ad6d780b64aa395944225150ba8fc61e381e0a4c
Oleksandr Mezhenskyi <oleksandr.mezhenskyi@chisw.com>
2021-08-09 23:02:14 +0300

    feat: add readable number from 100 to 999

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 module.exports = function toReadable (number) {
     const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numberOfHundreds;
+    let numberOfTens;
+    let numberOfUnits;
 
     if (number < 20 ) {
         return units[number];
-    }
-
-    if (number >= 20 &&  number < 100) {
-        const numberOfTens = Math.floor(number / 10);
-        const numberOfUnits = number % 10;
+    } else if (number >= 20 &&  number < 100) {
+        numberOfTens = Math.floor(number / 10);
+        numberOfUnits = number % 10;
 
         return numberOfUnits ? `${tens[numberOfTens - 2]} ${units[numberOfUnits]}` : tens[numberOfTens - 2];
+    } else {
+        numberOfHundreds = Math.floor(number / 100);
+        numberOfTens = Math.floor(number % 100 / 10);
+        numberOfUnits = number % 100 % 10;
+
+        if (!numberOfTens && !numberOfUnits) {
+            return `${units[numberOfHundreds]} hundred`;
+        } else if (numberOfTens < 2) {
+            return `${units[numberOfHundreds]} hundred ${units[number % 100]}`;
+        } else if (numberOfTens >= 2 && !numberOfUnits) {
+            return `${units[numberOfHundreds]} hundred ${tens[numberOfTens - 2]}`;
+        } else {
+            return `${units[numberOfHundreds]} hundred ${tens[numberOfTens - 2]} ${units[numberOfUnits]}`;
+        }
     }
 }
